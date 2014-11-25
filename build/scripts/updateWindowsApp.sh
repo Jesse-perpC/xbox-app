540be73d81e060dae18955bc077250d51a342d8e
XApp-Studio <asd28071977>
2014-11-25 12:25:23 +0100

    =

---
+++ build/scripts/updateWindowsApp.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -x
+SRC_DIR=../../DIST/
+
+DIST=../../../xbox-app-windows/xbox-app
+
+####### CLEAN UP
+rm -rf $DIST
+mkdir -p $DIST
+
+cp -rf $SRC_DIR/* $DIST/
+rm $DIST/xbox.zip
+
+
+cd $DIST/
+#find $DISTDIR -name *.git -exec rm '{}' ';'
+#git add -A
+#gitc
