790617149c33edbb591bc2c1fca9ceba170d3dcb
mc007 <asd28071977>
2014-11-26 19:57:50 +0100

    =

---
+++ build/scripts/updateAll.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -e
+
+# Base directory for this entire project
+BASEDIR=$(cd $(dirname $0) && pwd)/
+
+echo $BASEDIR
+
+# build dojo single file
+cd $BASEDIR/../../client/src
+#sh buildstandalone.sh
+gitc
+
+# commit xphp
+cd $BASEDIR/../../xapp
+gitc
+
+cd $BASEDIR/
+# update dist zip
+sh $BASEDIR/buildReleaseStandAlone.sh
+# update windows app
+sh $BASEDIR/updateWindowsApp.sh
+
+# commit root directory
+cd $BASEDIR/../../
+
+gitc
+
+
+
+
